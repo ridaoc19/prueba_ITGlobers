0399412959f42e85f0f7f429cc48f2dcca4e7b54
ridaoc19 <ridaoc19@gmail.com>
2022-10-20 09:55:13 +0200

    tamaño imagen articulos

--- client/src/component/sections/Articules.jsx
+++ client/src/component/sections/Articules.jsx
@@ -1,23 +1,22 @@
 // import { Link } from "react-router-dom";
-import style from "./sections.module.scss";
 import images from "../../scss/images/images.js";
 
 function Articules(props) {
   return (
     <div>
       <div className={`articules_container`}>
-        {images.icons.map(e => (
-            <>
-             <div key={e.name} className={`aticules_1_${e.name}`}>
+        {images.icons.map((e, i) => (
+          <>
+            <div className={`aticules_1_${e.name}`}>
               <img src={e.url1} alt="img" />
             </div>
             <div className={`aticules_2_${e.name}`}>
               <img src={e.url2} alt="img" />
-              <h3 >{e.name}</h3>
-              <h5 >{e.description}</h5>
+              <h3>{e.name}</h3>
+              <h5>{e.description}</h5>
               <a href="#">Detalle</a>
             </div>
-            </>
+          </>
         ))}
         {/* <Link to="/">Detalle</Link> */}
       </div>
