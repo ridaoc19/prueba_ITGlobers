51c7d78fb0189d10e48d13d0b12d1f4d76bdf80b
ridaoc19 <ridaoc19@gmail.com>
2022-10-20 04:07:05 +0200

    creacion de articulos

--- client/src/scss/images/images.js
+++ client/src/scss/images/images.js
@@ -1,27 +1,37 @@
 import { FaUmbrella } from "react-icons/fa";
 import { GiSchoolBag, GiSteampunkGoggles } from "react-icons/gi";
 import { MdChair } from "react-icons/md";
-import una from "./una.jpeg";
-import dos from "./dos.jpeg";
-import tres from "./tres.jpeg";
+
 
 // eslint-disable-next-line
 export default {
   icons: [
     {
       name: "PUFF",
+      url1: "https://acortar.link/aw4Qrg",
+      url2: "https://m.media-amazon.com/images/I/41UQ5DlsioL._AC_SX466_.jpg",
+      description: "Descripcion del producto, Este es un texto simulado",
       image: <FaUmbrella />,
     },
     {
       name: "RAIN",
+      url1: "https://acortar.link/mYkp1d",
+      url2: "https://m.media-amazon.com/images/I/71liLP7BWWL._AC_UX522_.jpg",
+      description: "Descripcion del producto, Este es un texto simulado",
       image: <GiSchoolBag />,
     },
     {
       name: "CART",
+      url1: "https://acortar.link/EOQwri",
+      url2: "https://m.media-amazon.com/images/I/517yDTr8-sL._AC_SY355_.jpg",
+      description: "Descripcion del producto, Este es un texto simulado",
       image: <MdChair />,
     },
     {
       name: "NAP",
+      url1: "https://acortar.link/NGgP1Q",
+      url2: "https://m.media-amazon.com/images/I/51WCb2YH3DS._AC_UX522_.jpg",
+      description: "Descripcion del producto, Este es un texto simulado",
       image: <GiSteampunkGoggles />,
     },
   ],
@@ -31,4 +41,6 @@ export default {
     "https://acortar.link/epopXD",
     "https://acortar.link/G6S3Bv",
   ],
+
+
 };
